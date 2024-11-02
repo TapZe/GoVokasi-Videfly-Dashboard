b6eb1a3a73ff6d6834d61168f62130e62352ccec
TapZe <nabilmuyassar@Gmail.com>
2024-11-02 22:57:35 +0800

    style: main layout init styling

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,7 +3,18 @@ import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
     <>
-      <Outlet />
+      <div className="flex flex-grow min-h-screen">
+        <div className="flex flex-row gap-x-4">
+          <div>
+            <div className="sidebar">This is the side bar</div>
+          </div>
+          <div className="flex flex-col flex-grow">
+            <div>this is the nav bar</div>
+            <div>this is the hero</div>
+            <Outlet />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
