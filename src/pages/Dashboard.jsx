1e6fbf701b2a9eb4947e86e3588a0e25aad684df
TapZe <nabilmuyassar@Gmail.com>
2024-11-02 22:54:29 +0800

    chore: initialize dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@nextui-org/react";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div>
+        <h1>Home Page</h1>
+        <Button color="primary">Button</Button>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
