37de0106bd989a00e83c146773f5f8b915611cca
TapZe <nabilmuyassar@Gmail.com>
2024-11-02 22:29:43 +0800

    chore: NextUI initialize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
+import { nextui } from "@nextui-org/react";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
