be1418c18182ea4659110cd251bbd50edc7c10ca
TapZe <nabilmuyassar@Gmail.com>
2024-11-02 22:53:05 +0800

    chore: nextui provider change place

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { NextUIProvider } from "@nextui-org/react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
 function App() {
   return (
     <>
-      <NextUIProvider>
-        <RouterProvider router={router} />
-      </NextUIProvider>
+      <RouterProvider router={router} />
     </>
   );
 }
