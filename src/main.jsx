cb3e627474b6520bcdaca57d5537a028d3ae4c2c
TapZe <nabilmuyassar@Gmail.com>
2024-11-03 21:07:49 +0800

    feat: add react redux and persist to main for provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import App from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NextUIProvider>
+          <App />
+        </NextUIProvider>
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
