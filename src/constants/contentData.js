0dd57ccb5fe503bd3ca4490f2488baa1d64065e0
Ghifary Adinegoro <ghifaryadinegoro555@gmail.com>
2024-11-07 16:24:10 +0700

    fix: insert image to placeholder

--- src/constants/contentData.js
+++ src/constants/contentData.js
@@ -3,7 +3,8 @@ export const contentCardData = [
     id: 1,
     title: "Alchemist Fragrance",
     typeDesc: "Text to Video",
-    image: "https://placehold.co/600x400/png",
+    image:
+      "https://images.unsplash.com/photo-1598121496608-3d0b9627155e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     success: false,
     duration: 60,
     progress: 65,
@@ -14,7 +15,8 @@ export const contentCardData = [
     id: 2,
     title: "Mystic Forest",
     typeDesc: "Text to Video",
-    image: "https://placehold.co/600x400/png",
+    image:
+      "https://images.unsplash.com/photo-1619880383237-66c9ac98ea16?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     success: false,
     duration: 120,
     progress: 0,
@@ -25,7 +27,8 @@ export const contentCardData = [
     id: 3,
     title: "Ocean Waves",
     typeDesc: "Text to Video",
-    image: "https://placehold.co/600x400/png",
+    image:
+      "https://images.unsplash.com/photo-1613857794632-052edbb98d58?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     success: false,
     duration: 90,
     progress: 45,
@@ -36,7 +39,8 @@ export const contentCardData = [
     id: 4,
     title: "City Lights",
     typeDesc: "Text to Video",
-    image: "https://placehold.co/600x400/png",
+    image:
+      "https://images.unsplash.com/photo-1499174549139-68d3f37243b4?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     success: false,
     duration: 180,
     progress: 0,
@@ -47,7 +51,8 @@ export const contentCardData = [
     id: 5,
     title: "Starry Night",
     typeDesc: "Text to Video",
-    image: "https://placehold.co/600x400/png",
+    image:
+      "https://images.unsplash.com/photo-1531808607305-42041f13cc0e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     success: false,
     duration: 75,
     progress: 55,
@@ -58,7 +63,8 @@ export const contentCardData = [
     id: 6,
     title: "Desert Mirage",
     typeDesc: "Text to Video",
-    image: "https://placehold.co/600x400/png",
+    image:
+      "https://images.unsplash.com/photo-1612852098525-62362dd2745d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     success: false,
     duration: 150,
     progress: 0,
@@ -69,7 +75,8 @@ export const contentCardData = [
     id: 7,
     title: "Mountain Peaks",
     typeDesc: "Text to Video",
-    image: "https://placehold.co/600x400/png",
+    image:
+      "https://images.unsplash.com/photo-1520987314741-180bfb8b4791?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     success: false,
     duration: 60,
     progress: 0,
@@ -80,7 +87,8 @@ export const contentCardData = [
     id: 8,
     title: "Rainy Days",
     typeDesc: "Text to Video",
-    image: "https://placehold.co/600x400/png",
+    image:
+      "https://images.unsplash.com/photo-1599840676968-37cdae296247?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     success: true,
     duration: 120,
     progress: 100,
@@ -91,7 +99,8 @@ export const contentCardData = [
     id: 9,
     title: "Calm Waters",
     typeDesc: "Text to Video",
-    image: "https://placehold.co/600x400/png",
+    image:
+      "https://images.unsplash.com/photo-1730831334853-813878bcb46d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     success: false,
     duration: 100,
     progress: 0,
@@ -102,7 +111,8 @@ export const contentCardData = [
     id: 10,
     title: "Urban Jungle",
     typeDesc: "Text to Video",
-    image: "https://placehold.co/600x400/png",
+    image:
+      "https://images.unsplash.com/photo-1633129282263-179eeee0ba5f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     success: false,
     duration: 110,
     progress: 0,
@@ -113,7 +123,8 @@ export const contentCardData = [
     id: 11,
     title: "Whimsical Dreams",
     typeDesc: "Text to Video",
-    image: "https://placehold.co/600x400/png",
+    image:
+      "https://images.unsplash.com/photo-1516060610219-308fc12a5e37?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     success: false,
     duration: 80,
     progress: 0,
